7f506b09d3e7019462b728c684fdace9b2e820bf
yashwant mergewar <yashwantmergewar@gmail.com>
2025-06-03 22:40:37 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: "./.env" });
 
 connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection error:", error);
+})
     
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// using promise to handle async errors in express
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error)=> next(error));
+    }
+}
+
+export {asyncHandler}
+
+// By using try catch, we can handle errors in our async functions and pass them to the next middleware
+
+// const asyncHandler= () => {}
+// const asyncHandler = () => () => {}
+// const asyncHandler = () => async () =>{}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message 
+//         })
+//     }
+// }
