136adf9664a9c194096a0a2f7078655cdf79aef6
yashwant mergewar <yashwantmergewar@gmail.com>
2025-06-04 19:26:14 +0530

    user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // URL from cloudinary
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true // URL from cloudinary
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    decription: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true});
+
+export const Video = mongoose.model("Video", videoSchema);
