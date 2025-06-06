2394700e19a1ce64d1cfe378c0ad011589782631
yashwant mergewar <yashwantmergewar@gmail.com>
2025-06-06 08:46:54 +0530

    set up cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { response } from 'express';
+import fs from 'fs';
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        // file has been uploaded successfully
+        console.log("file uploaded on cloudinary: ", response.url);
+        return response.url;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload op failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
