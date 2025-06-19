d67d651415b93d80ec158f9c76b78d0610b9f201
yashwant mergewar <yashwantmergewar@gmail.com>
2025-06-19 13:39:35 +0530

    Access Refresh Token, middleware, are added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+// Creating method for access and refresh token
+const generateAccessAndRefreshToken =async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        // Here we have taken access and refresh token from the user model
+        // now we have to save the refresh token in the database
+        // and return the access token and refresh token
+        
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false}) // we are not validating the user before saving the refresh token (not asking for password again)
+
+        return {accessToken, refreshToken};
+
+    } catch (error) {
+        throw new ApiError(500, "Error while generating access and refresh token");
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
     // Steps to register a user:
@@ -35,21 +55,32 @@ const registerUser = asyncHandler(async (req, res) => {
     } 
 
     // Check if the user already exists in the database
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
-    console.log("Existed User: ", existedUser);
+    // console.log("Existed User: ", existedUser);
     
 
     if (existedUser){
         throw new ApiError(409, "User already exists with this username or email");
     }
 
+    console.log(req.files);
+    
+
     // check for image and avatar
     // multer gives us the files access through req.files
     // it might be or might not be present the files thats why we chain the optional chaining operator
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;        
+    }
+
+    console.log("Avatar Local Path: ", avatarLocalPath);
+    console.log("Cover Image Local Path: ", coverImageLocalPath);
 
     if (!avatarLocalPath || !coverImageLocalPath) {
         throw new ApiError(400, "Avatar and Cover Image are required");
@@ -58,6 +89,10 @@ const registerUser = asyncHandler(async (req, res) => {
     // upload them to cloudinary, avatar
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    console.log("Avatar: ", avatar);
+    console.log("Cover Image: ", coverImage);
+    
 
     if (!avatarLocalPath || !coverImageLocalPath) {
         throw new ApiError(400, "Avatar and Cover Image are required");
@@ -69,7 +104,7 @@ const registerUser = asyncHandler(async (req, res) => {
         username: username.toLowerCase(),
         password,
         avatar: avatar.url,
-        coverImage: coverImage.url
+        coverImage: coverImage?.url || "", // coverImage is optional, so we can set it to an empty string if not provided
     })
 
     // we can do it by undefining the password and refreshToken fields
@@ -88,4 +123,101 @@ const registerUser = asyncHandler(async (req, res) => {
     )
 })
 
-export {registerUser};
+const loginUser = asyncHandler(async (req, res)=> {
+    // Login TODOS
+    // Step 1: Get the user data from frontend
+    // Step 2: Validate the user data
+    // Step 3: Check if user exists in the database if not throw error
+    // Step 4: Check if the password is correct
+    // Step 5: Generate access token and refresh token
+    // Step 6: Update the refresh token in the database
+    // Step 7: Send cookies
+
+    // Step 1: Get the user data from frontend
+    const {username, email, password} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400, "Username or Email are required");
+    }
+
+    //Step 2: Validate the user data
+    // this is a mongoDB query to find a user by username or email
+    // finedOne is a method provided by mongoose to find a single document in the database which matches first argument
+    // $or is a mongoDB operator
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    //Step 3: Check if user exists in the database if not throw error
+    if (!user) {
+        throw new ApiError(404, "User not found with this username or email");
+    }
+
+
+    // Step 4: Check if the password is correct
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password");
+    }
+
+    // Step 5: Generate access token and refresh token
+    // This generateAccessAndRefreshToken method returning access and refresh token and we simply destructuring it like req.body
+    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    // Now we have reference of user in the database of these outerscope user which is empty
+    // so we again take database call for user to get the user details without password and refresh token
+    const loggedInUser = await User.findById(user._id).select("-password -refereshToken");
+    // Now loggedInUser have all the user details except password and refresh token
+    // and also we have access token and refresh token of the user separately
+
+    //Step 7: Send cookies
+    const options = {
+        httpOnly: true, // this means the cookie is not accessible from client side JavaScript
+        secure: true, // this means the cookie is only sent over HTTPS
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, 
+            {
+                user: loggedInUser, // this is the user object without password and refresh token
+                accessToken, // this is the access token
+                refreshToken // this is the refresh token
+            }
+            , "User logged in successfully")
+    )
+
+        const loggedOutUser = asyncHandler(async (req, res) => {})
+
+})
+
+const logOutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {refreshToken: undefined}
+        },
+        {
+            new: true // this will return the updated user
+        }
+    )
+
+    const options = {
+        httpOnly: true, // this means the cookie is not accessible from client side JavaScript
+        secure: true, // this means the cookie is only sent over HTTPS
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options) // clear the access token cookie
+    .clearCookie("refreshToken", options) // clear the refresh token cookie
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logOutUser
+};
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,52 @@
 import {v2 as cloudinary} from 'cloudinary';
 import { response } from 'express';
 import fs from 'fs';
+import path from 'path';
 
 cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+        api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
     });
 
 const uploadOnCloudinary = async (localFilePath) => {
+    console.log("Uploading file to Cloudinary: ", localFilePath);
+    
     try {
-        if(!localFilePath) return null;
+        if(!localFilePath){
+            return null;
+        }
+
+        // Fix windows file path issues: converting backslashes to forward slashes
+        // This is important because Cloudinary expects forward slashes in the file path
+        // This is a Windows-style path with backslashes (\), 
+        // but Cloudinary (and most Node.js tools that interact with external services) 
+        //expects UNIX-style paths with forward slashes (/).
+        const fixedFilePath = path.resolve(localFilePath).replace(/\\/g, '/');
+
         // upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
+        const response = await cloudinary.uploader.upload(fixedFilePath, {
             resource_type: 'auto'
         })
+        console.log("Cloudinary Response: ", response);
+        
+        // console.log("Exists?", fs.existsSync("public/temp/image.jpg"));
+
+
         // file has been uploaded successfully
-        console.log("file uploaded on cloudinary: ", response.url);
-        return response.url;
+        return {
+            url: response.secure_url, // secure_url is the URL of the uploaded file
+            public_id: response.public_id // public_id is the unique identifier for the uploaded file
+        }
+
+        
+        // console.log("file uploaded on cloudinary: ", response.url);
+        // return response.url;
         
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload op failed
+        console.log("Error",error);
+        
+        if (localFilePath) fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload op failed
         return null;
     }
 }
