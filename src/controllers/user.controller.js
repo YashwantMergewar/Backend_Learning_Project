b1db09155346d8b6c029691f3643e96d07f3f5b3
yashwant mergewar <yashwantmergewar@gmail.com>
2025-10-02 16:55:31 +0530

    again same process

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -472,7 +472,6 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         }
     ])
 
-    console.log("Channel Profile: ", channel);
 
     if (!channel?.length) {
         throw new ApiError(404, "Channel not found with this username");
